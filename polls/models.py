e568378d60c22c7cdc72e7b0ebf562b4c574ad3d
Василий <kolesnikovasiliy@yandex.ru>
2021-10-04 18:00:28 +0300

    модели

--- polls/models.py
+++ polls/models.py
@@ -28,8 +28,8 @@ class Question(models.Model):
         choices = QUESTION_TYPE_CHOICES,
         verbose_name = 'Тип вопроса',
     )
-    option0 = models.CharField(max_length = 64, verbose_name = 'Вариант ответа А', null = True, blank = True)
     option1 = models.CharField(max_length = 64, verbose_name = 'Вариант ответа Б', null = True, blank = True)
+    option0 = models.CharField(max_length = 64, verbose_name = 'Вариант ответа А', null = True, blank = True)
     option2 = models.CharField(max_length = 64, verbose_name = 'Вариант ответа В', null = True, blank = True)
     option3 = models.CharField(max_length = 64, verbose_name = 'Вариант ответа Г', null = True, blank = True)
     
