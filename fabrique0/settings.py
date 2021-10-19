37c537f1df0795857206e445f6ae46c9a3c3103b
Василий <kolesnikovasiliy@yandex.ru>
2021-10-19 18:16:05 +0300

    Разрешение запросов с других доменов

--- fabrique0/settings.py
+++ fabrique0/settings.py
@@ -208,6 +208,15 @@ LOGGING = {
     'disable_existing_loggers': True,
 }
 
+CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    'https://vote-4-vacya.herokuapp.com',
+] # If this is used, then not need to use `CORS_ORIGIN_ALLOW_ALL = True`
+CORS_ORIGIN_REGEX_WHITELIST = [
+    'https://vote-4-vacya.herokuap.com',
+]
+
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 CSRF_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
