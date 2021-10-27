95a86f2f95a29778a571d3c28eab3536e8348cb7
Василий <kolesnikovasiliy@yandex.ru>
2021-10-27 18:20:31 +0300

    Использование сессий django

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import index, api_polls, api_questions, api_answers
 
 urlpatterns = [
-	path('', index),
+    path('', index),
     path('api/polls/', api_polls),
     path('api/questions/', api_questions),
     path('api/answers/', api_answers),
