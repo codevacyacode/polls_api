d18747d55f061173f5f04732ada571ad63950915
Василий <kolesnikovasiliy@yandex.ru>
2021-11-09 17:53:21 +0300

    Добавлены маршруты

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import index, api_polls, api_questions, api_answers
+from .views import index, api_polls, api_questions, api_answers, api_whole_poll
 
 urlpatterns = [
     path('', index),
+    path('api/polls/<int:pk>/', api_whole_poll),
     path('api/polls/', api_polls),
     path('api/questions/', api_questions),
     path('api/answers/', api_answers),
